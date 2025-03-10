9fc86970d7d095e1bbdb0ba1d6cfebd206d91951
Israel Barmack <israel.bar.dev@gmail.com>
2025-03-10 19:13:49 +0200

    Add CORS support and update dependencies in FastAPI project

--- src/main.py
+++ src/main.py
@@ -2,10 +2,17 @@ import logging
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from .payments import router
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://adrateliyahu.com", "https://hsf0dg-uz.myshopify.com"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
