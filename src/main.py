a36d1730375cc3dc1f0809852c4f898340541298
Israel Barmack <israel.bar.dev@gmail.com>
2025-03-17 10:22:12 +0200

    Refactor CORS middleware configuration for improved readability

--- src/main.py
+++ src/main.py
@@ -10,8 +10,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://adrateliyahu.com", "https://hsf0dg-uz.myshopify.com"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
