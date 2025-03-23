f7a7a3e0bc519b3c0aa5f619df4544d8706ffa3f
Israel Barmack <israel.bar.dev@gmail.com>
2025-03-23 13:02:02 +0200

    Remove unused set_cursor import and update rate limiting error messages for endpoints

--- src/payments/__init__.py
+++ src/payments/__init__.py
@@ -1,5 +1,4 @@
 from .constants import ACCESS_TOKEN, API_URL, URL, API_VERSION
 from .router import router
-from .utils import set_cursor
 
 __all__ = ["ACCESS_TOKEN", "API_URL", "URL", "API_VERSION", "router", "set_cursor"]
